dae7be1977ed94ab78a19d253b1c48d9050365d0
Athashree <athashreeav5556@gmail.com>
2020-08-27 12:27:27 +0530

    linked pages

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,17 @@
 //here the component(function) name can be anything but it must be 
 //exported as "default"
+import Link from 'next/link'
+
+
 export default function FirstPost(){
-	return <h1> This is First Post</h1>
+	return (
+		<>
+		<h1>This is First Post</h1>
+		<h2>
+			<Link href="/">
+				<a>Back to home</a>
+			</Link>
+		</h2>
+		</>
+		)
 }
